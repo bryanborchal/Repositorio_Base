5b41416607f2bc2114b09065194b19f49e8f9f9e
bryanborchal <bryanborchal597@gmail.com>
2025-09-10 10:55:05 -0300

    Add files via upload

---
+++ Modulo 2/Exercicios/ex_2-1.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('ex_2-1.html')
+
+@app.route('/sobre')
+def sobre():
+    return 'Fábrica de fazer programa!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
